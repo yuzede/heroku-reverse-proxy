06cf5dfc267fdd77610a53c0645f42738003ffb0
Christian Kohlberg <20028877+ChristianKohlberg@users.noreply.github.com>
2023-05-12 18:59:00 +0200

    fix(nginx): update code for nginx and laravel-echo-server

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bash -c "envsubst '${PORT}' < nginx.conf.template > /etc/nginx/nginx.conf" && service nginx start
+envsubst '${PORT}' < nginx.conf.template > /etc/nginx/nginx.conf && service nginx start
 bash -c "chmod +x extract_env.sh && bash extract_env.sh && sleep 1" && laravel-echo-server start

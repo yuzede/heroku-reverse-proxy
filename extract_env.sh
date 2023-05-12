8f25de01d5ef0567bb402981aaa0340d292c7bae
Christian Kohlberg <christian.kohlberg@gmail.com>
2023-05-12 12:25:31 +0200

    feat(nginx): add socket

---
+++ extract_env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+
+echo "script is running"
+# Extract REDIS_URL components
+REDIS_URL=${REDIS_URL}
+REDIS_PASSWORD=$(echo "$REDIS_URL" | awk -F':' '{print $3}' | awk -F'@' '{print $1}')
+REDIS_HOST=$(echo "$REDIS_URL" | awk -F'@' '{print $2}' | awk -F':' '{print $1}')
+REDIS_PORT=$(echo "$REDIS_URL" | awk -F':' '{print $NF}')
+
+# Create .env file
+echo "LARAVEL_ECHO_SERVER_REDIS_PASSWORD=${REDIS_PASSWORD}" >> .env
+echo "LARAVEL_ECHO_SERVER_REDIS_HOST=${REDIS_HOST}" >> .env
+echo "LARAVEL_ECHO_SERVER_REDIS_PORT=${REDIS_PORT}" >> .env
+echo "LARAVEL_ECHO_SERVER_AUTH_HOST=${AUTH_HOST}" >> .env
+echo "LARAVEL_ECHO_SERVER_PORT=${PORT}" >> .env

bc132ba1e47494620c3e80533e80ea5ed36a294e
Christian Kohlberg <christian.kohlberg@gmail.com>
2023-05-15 11:46:06 +0200

    refactor(lb): make it point to staging or production depending on env

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-envsubst '${PORT}' < nginx.conf.template > /etc/nginx/nginx.conf && service nginx start
+envsubst '${PORT} ${YUZE_API_HOST}' < nginx.conf.template > /etc/nginx/nginx.conf && service nginx start
 bash -c "chmod +x extract_env.sh && bash extract_env.sh && sleep 1" && laravel-echo-server start
